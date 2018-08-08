b824833b9f6bde3622773202a8dc470978e0a565
mrMigles <delta_001@bk.ru>
2018-08-08 23:01:00 +0400

    Correct address of backend url for prod

--- ping-service-web/src/app/backend-url.interceptor.ts
+++ ping-service-web/src/app/backend-url.interceptor.ts
@@ -8,7 +8,7 @@ export class BackendUrlInterceptor implements HttpInterceptor {
   /* constructor(@Inject('BASE_URL') private baseUrl: string) {} */
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const url = 'http://localhost:8080';
+    const url = window.location.origin + '/';
     req = req.clone({
       url: url + req.url,
       withCredentials: true
--- ping-service/src/main/java/ru/holyway/pingservice/config/WebConfig.java
+++ ping-service/src/main/java/ru/holyway/pingservice/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Value("${cors.allowed-origins}")
